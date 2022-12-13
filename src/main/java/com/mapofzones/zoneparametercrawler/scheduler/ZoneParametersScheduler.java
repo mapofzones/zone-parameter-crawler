4fa10e7d28934bcd941bd1a9b1f3f73eb0641dfe
Wolthrax <wolthrax@gmail.com>
2022-12-13 18:55:57 +0300

    Fix scheduler

--- src/main/java/com/mapofzones/zoneparametercrawler/scheduler/ZoneParametersScheduler.java
+++ src/main/java/com/mapofzones/zoneparametercrawler/scheduler/ZoneParametersScheduler.java
@@ -15,20 +15,20 @@ public class ZoneParametersScheduler {
         this.zoneParametersFacadeProxy = zoneParametersFacadeProxy;
     }
 
-    //@Scheduled(cron = "1 0 * * * *")
-    @Scheduled(fixedDelayString = "3600000", initialDelay = 0)
+    @Scheduled(cron = "1 0 * * * *")
+//    @Scheduled(fixedDelayString = "3600000", initialDelay = 0)
     public void createEmptyZoneParameters() {
         this.zoneParametersFacadeProxy.createEmptyZoneParameters();
     }
 
-    //@Scheduled(cron = "30 0 * * * *")
-    @Scheduled(fixedDelayString = "3600000", initialDelay = 3000)
+    @Scheduled(cron = "30 0 * * * *")
+//    @Scheduled(fixedDelayString = "3600000", initialDelay = 3000)
     public void callFindBaseZoneParameters() {
         this.zoneParametersFacadeProxy.findBaseZoneParameters();
     }
 
-    //@Scheduled(cron = "0 2 * * * *")
-    @Scheduled(fixedDelayString = "3600000", initialDelay = 5000)
+    @Scheduled(cron = "0 2 * * * *")
+//    @Scheduled(fixedDelayString = "3600000", initialDelay = 5000)
     public void callFindDelegationsAmount() {
         this.zoneParametersFacadeProxy.findDelegationsAmount();
     }
